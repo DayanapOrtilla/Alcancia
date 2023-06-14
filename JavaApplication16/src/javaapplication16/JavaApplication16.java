4cd94bd00aa55256e487edb00fde2912b36ca4c1
Dayana Potilla <carmenportilla@unicauca.edu.co>
2023-06-13 21:28:48 -0500

    primer commit

---
+++ JavaApplication16/src/javaapplication16/JavaApplication16.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication16;
+
+/**
+ *
+ * @author DAYANA PORTILLA
+ */
+public class JavaApplication16 {
+
+    public static void main(String[] args) {
+        System.out.println("kk");
+    }
+    
+}
